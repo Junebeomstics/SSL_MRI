0192d391ddcfa8cc8c05027ecb1c9ee7fdbf6eaa
yeonchae <64460370+yeonchae@users.noreply.github.com>
2022-05-02 17:57:47 +0900

    Update simclr.py

--- SimCLR_yeon/simclr.py
+++ SimCLR_yeon/simclr.py
@@ -1,45 +1,38 @@
 import logging
 import os
 import sys
-from torch.nn import DataParallel
 import torch
 import torch.nn.functional as F
 from torch.cuda.amp import GradScaler, autocast
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from utils.accuracy import accuracy
+from utils import save_config_file, accuracy, save_checkpoint
 
 torch.manual_seed(0)
 
 
 class SimCLR(object):
 
-    def __init__(self, net, loader_train,loss, config, scheduler=None):
-
-        super().__init__()
-        self.writer = SummaryWriter()
-        logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
+    def __init__(self, loss, model, config, loader_train, loader_val, optimizer, scheduler, batch_size = 256, n_views = 2, temperature = 0.07):
         self.device = torch.device("cuda")
-        self.criterion = torch.nn.CrossEntropyLoss().to(self.device)
-        self.logger = logging.getLogger("yAwareCL")
-        self.model = net
         self.loss = loss
-        self.optimizer = torch.optim.Adam(net.parameters(), lr=config.lr, weight_decay=config.weight_decay)
+        self.model = model.to(self.device)
+        self.optimizer = optimizer
         self.scheduler = scheduler
-        self.loader = loader_train
-        if config.cuda and not torch.cuda.is_available():
-            raise ValueError("No GPU found: set cuda=False parameter.")
+        self.writer = SummaryWriter()
         self.config = config
+        self.loader = loader_train
+        self.loader_val = loader_val
+        self.temperature = temperature
+        self.n_views = n_views
+        self.batch_size = batch_size
+        logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
+        self.criterion = torch.nn.CrossEntropyLoss().to(self.device)
         self.metrics = {}
-
-        if hasattr(config, 'pretrained_path') and config.pretrained_path is not None:
-            self.load_model(config.pretrained_path)
-
-        self.model = DataParallel(self.model).to(self.device)
 
     def info_nce_loss(self, features):
 
-        labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
+        labels = torch.cat([torch.arange(self.batch_size) for i in range(self.n_views)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
         labels = labels.to(self.device)
 
@@ -65,12 +58,12 @@ class SimCLR(object):
         logits = torch.cat([positives, negatives], dim=1)
         labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.device)
 
-        logits = logits / self.args.temperature
+        logits = logits / self.temperature
         return logits, labels
 
     def train(self, train_loader):
-
-        scaler = GradScaler(enabled=self.args.fp16_precision)
+        print(self.loss)
+        print(self.optimizer)
 
         n_iter = 0
         logging.info(f"Start SimCLR training for {self.config.nb_epochs} epochs.")
@@ -78,26 +71,23 @@ class SimCLR(object):
         for epoch_counter in range(self.config.nb_epochs):
             self.model.train()
             nb_batch = len(self.loader)
-            training_loss = []
+            training_loss = 0
             pbar = tqdm(total=nb_batch, desc="Training")
+            
             for images, _ in tqdm(train_loader):
-                images = torch.cat(images, dim=0)
-
+                pbar.update()
                 images = images.to(self.device)
-
-                with autocast(enabled=self.args.fp16_precision):
-                    features = self.model(images)
-                    logits, labels = self.info_nce_loss(features)
-                    loss = self.criterion(logits, labels)
-
+                self.optimizer.zero_grad()
+                
+                images = self.model(images[:, 0, :])
+                
+                logits, labels = self.info_nce_loss(images)
+                loss = self.criterion(logits, labels)
+                training_loss += float(loss) / nb_batch
                 self.optimizer.zero_grad()
 
-                scaler.scale(loss).backward()
 
-                scaler.step(self.optimizer)
-                scaler.update()
-
-                if n_iter % self.args.log_every_n_steps == 0:
+                if n_iter % 100 == 0:
                     top1, top5 = accuracy(logits, labels, topk=(1, 5))
                     self.writer.add_scalar('loss', loss, global_step=n_iter)
                     self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
@@ -105,45 +95,8 @@ class SimCLR(object):
                     self.writer.add_scalar('learning_rate', self.scheduler.get_lr()[0], global_step=n_iter)
 
                 n_iter += 1
-
-            # warmup for the first 10 epochs
-            if epoch_counter >= 10:
-                self.scheduler.step()
-            logging.debug(f"Epoch: {epoch_counter}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
-
-        logging.info("Training has finished.")
-        # save model checkpoints
-        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.args.epochs)
-        torch.save({
-            'epoch': self.args.epochs,
-            'arch': self.args.arch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
-        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
-    
-    def pretraining(self):
-
-        for epoch in range(self.config.nb_epochs):
-
-            ## Training step
-            self.model.train()
-            nb_batch = len(self.loader)
-            training_loss = 0
-            pbar = tqdm(total=nb_batch, desc="Training")
-            for (inputs, labels) in self.loader:
-                pbar.update()
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                self.optimizer.zero_grad()
-                z_i = self.model(inputs[:, 0, :])
-                z_j = self.model(inputs[:, 1, :])
-                batch_loss, logits, target = self.loss(z_i, z_j, labels)
-                batch_loss.backward()
-                self.optimizer.step()
-                training_loss += float(batch_loss) / nb_batch
             pbar.close()
-
+            
             ## Validation step
             nb_batch = len(self.loader_val)
             pbar = tqdm(total=nb_batch, desc="Validation")
@@ -154,94 +107,38 @@ class SimCLR(object):
                 for (inputs, labels) in self.loader_val:
                     pbar.update()
                     inputs = inputs.to(self.device)
-                    labels = labels.to(self.device)
-                    z_i = self.model(inputs[:, 0, :])
-                    z_j = self.model(inputs[:, 1, :])
-                    batch_loss, logits, target = self.loss(z_i, z_j, labels)
-                    val_loss += float(batch_loss) / nb_batch
+                    images = self.model(inputs[:, 0, :])
+                    logits, labels = self.info_nce_loss(images)
+                    loss = self.criterion(logits, labels)
+                    self.optimizer.zero_grad()
+                    val_loss += float(loss) / nb_batch
                     for name, metric in self.metrics.items():
                         if name not in val_values:
                             val_values[name] = 0
-                        val_values[name] += metric(logits, target) / nb_batch
+                        val_values[name] += metric(logits, labels) / nb_batch
             pbar.close()
 
             metrics = "\t".join(["Validation {}: {:.4f}".format(m, v) for (m, v) in val_values.items()])
             print("Epoch [{}/{}] Training loss = {:.4f}\t Validation loss = {:.4f}\t".format(
-                epoch+1, self.config.nb_epochs, training_loss, val_loss)+metrics, flush=True)
+                epoch_counter+1, self.config.nb_epochs, training_loss, val_loss)+metrics, flush=True)
 
             if self.scheduler is not None:
                 self.scheduler.step()
 
-            if (epoch % self.config.nb_epochs_per_saving == 0 or epoch == self.config.nb_epochs - 1) and epoch > 0:
+            if (epoch_counter % self.config.nb_epochs_per_saving == 0 or epoch_counter == self.config.nb_epochs - 1) and epoch_counter > 0:
                 torch.save({
-                    "epoch": epoch,
+                    "epoch": epoch_counter,
                     "model": self.model.state_dict(),
                     "optimizer": self.optimizer.state_dict()},
                     os.path.join(self.config.checkpoint_dir, "{name}_epoch_{epoch}.pth".
-                                 format(name="y-Aware_Contrastive_MRI", epoch=epoch)))
+                                 format(name="y-Aware_Contrastive_MRI", epoch=epoch_counter)))
 
-
-    def fine_tuning(self):
-        print(self.loss)
-        print(self.optimizer)
-
-        for epoch in range(self.config.nb_epochs):
-            ## Training step
-            self.model.train()
-            nb_batch = len(self.loader)
-            training_loss = []
-            pbar = tqdm(total=nb_batch, desc="Training")
-            for (inputs, labels) in self.loader:
-                pbar.update()
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                self.optimizer.zero_grad()
-                y = self.model(inputs)
-                batch_loss = self.loss(y,labels)
-                batch_loss.backward()
-                self.optimizer.step()
-                training_loss += float(batch_loss) / nb_batch
-            pbar.close()
-
-            ## Validation step
-            nb_batch = len(self.loader_val)
-            pbar = tqdm(total=nb_batch, desc="Validation")
-            val_loss = 0
-            with torch.no_grad():
-                self.model.eval()
-                for (inputs, labels) in self.loader_val:
-                    pbar.update()
-                    inputs = inputs.to(self.device)
-                    labels = labels.to(self.device)
-                    y = self.model(inputs)
-                    batch_loss = self.loss(y, labels)
-                    val_loss += float(batch_loss) / nb_batch
-            pbar.close()
-
-            print("Epoch [{}/{}] Training loss = {:.4f}\t Validation loss = {:.4f}\t".format(
-                epoch+1, self.config.nb_epochs, training_loss, val_loss), flush=True)
-
-            if self.scheduler is not None:
-                self.scheduler.step()
-
-
-    def load_model(self, path):
-        checkpoint = None
-        try:
-            checkpoint = torch.load(path, map_location=lambda storage, loc: storage)
-        except BaseException as e:
-            self.logger.error('Impossible to load the checkpoint: %s' % str(e))
-        if checkpoint is not None:
-            try:
-                if hasattr(checkpoint, "state_dict"):
-                    unexpected = self.model.load_state_dict(checkpoint.state_dict())
-                    self.logger.info('Model loading info: {}'.format(unexpected))
-                elif isinstance(checkpoint, dict):
-                    if "model" in checkpoint:
-                        unexpected = self.model.load_state_dict(checkpoint["model"], strict=False)
-                        self.logger.info('Model loading info: {}'.format(unexpected))
-                else:
-                    unexpected = self.model.load_state_dict(checkpoint)
-                    self.logger.info('Model loading info: {}'.format(unexpected))
-            except BaseException as e:
-                raise ValueError('Error while loading the model\'s weights: %s' % str(e))
+        # save model checkpoints
+        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.config.nb_epochs)
+        save_checkpoint({
+            'epoch': self.config.nb_epochs,
+            'arch': 'resnet18',
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
+        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
